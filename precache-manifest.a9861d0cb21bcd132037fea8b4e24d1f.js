731ce57a5e24eec2b52db1e843c5772b38d8b6f1
swapnil-shah <swapnilshah.me@gmail.com>
2020-09-17 16:53:59 -0400

    Updates

---
+++ precache-manifest.a9861d0cb21bcd132037fea8b4e24d1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09204e1580e2954b8d00df66f4282ff5",
+    "url": "/github.io/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "06e0a9763e79ab75ca4c",
+    "url": "/github.io/react-monsters-rolodex/static/css/main.99d4a2a0.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/github.io/react-monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/react-monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e0a9763e79ab75ca4c",
+    "url": "/github.io/react-monsters-rolodex/static/js/main.1baded35.chunk.js"
+  },
+  {
+    "revision": "d3fcdc671e3878e7678e",
+    "url": "/github.io/react-monsters-rolodex/static/js/runtime-main.2a87891d.js"
+  }
+]);
